f001981c84fb4c2ac02b9c35aa9493d92d5847a0
pgraeson <30897496+pgraeson@users.noreply.github.com>
2017-10-20 13:35:50 +1100

    Add files via upload

---
+++ Mona.ino
@@ -0,0 +1,27 @@
+const int buzzer = 9;
+
+void setup() {
+
+  Serial.begin(9600);
+  pinMode(6, INPUT);
+
+  pinMode(buzzer, OUTPUT);
+}
+
+void loop() {
+ 
+  if (digitalRead(6) == HIGH) {
+    Serial.println("Active");
+   
+    tone(buzzer, 1000);
+    delay(500);
+    noTone(buzzer);
+    delay(500);
+  }
+  else {
+    Serial.println("Inactive");
+  }
+
+
+
+}
